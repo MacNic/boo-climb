1c489880a5b4f60a17074d6948fd711b02b34c1c
Nicolas Marique <nicolasmarique@gmail.com>
2018-11-29 19:35:50 +0100

    Adding file to make the report

---
+++ Fiche/Step1.py
@@ -0,0 +1,40 @@
+# Si vous utilisez mu editor, commentez
+# la ligne 'import pgzrun'
+#import pgzrun
+
+TITLE   = 'Boo climb'
+WIDTH   = 1366
+HEIGHT  = 768
+
+# Description
+#   Cette function est appelée par le moteur
+#   pygame zero pour mettre à jour les paramètres
+#   des différents objets qui composent le jeu a une
+#   fréquence d'environ 50 ou 60 fois par secondes
+#
+# Paramètres
+#   Aucun
+#
+# Retourne
+#   Rien
+#        
+def update() :
+    return
+    
+# Description
+#   Cette fonction est appelée par le moteur
+#   pygame zero lorsqu'il détecte que l'interface
+#   a besoin d'être redessinée
+#
+# Paramètres
+#   Aucun
+#
+# Retourne
+#   Rien
+#            
+def draw():
+    return 
+    
+# Si vous utilisez mu editor, commentez
+# la ligne 'pgzrun.go()'
+#pgzrun.go()
